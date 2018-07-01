2bc8d2cbfe650d6f029f6a7cff647aa0b0d4c08a
antonTroyan <32644076+antonTroyan@users.noreply.github.com>
2018-07-01 15:27:55 +0300

    Initial commit

---
+++ src/main/java/com/mkyong/rest/Chat.java
@@ -0,0 +1,47 @@
+package com.mkyong.rest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Path("/chat")
+public class Chat {
+
+    final static Map<String, AsyncResponse> waiters = new ConcurrentHashMap<>();
+    final static ExecutorService ex = Executors.newSingleThreadExecutor();
+
+    @Path("/{nick}")
+    @GET
+    @Produces("text/plain")
+    public void hangUp(@Suspended AsyncResponse asyncResp, @PathParam("nick") String nick) {
+
+        waiters.put(nick, asyncResp);
+    }
+
+    @Path("/{nick}")
+    @POST
+    @Produces("text/plain")
+    @Consumes("text/plain")
+    public String sendMessage(final @PathParam("nick") String nick, final String message) {
+
+        ex.submit(new Runnable() {
+            @Override
+            public void run() {
+                Set<String> nicks = waiters.keySet();
+                for (String n : nicks) {
+// Sends message to all, except sender
+                    if (!n.equalsIgnoreCase(nick))
+                        waiters.get(n).resume(nick + " said that: " + message);
+                }
+            }
+        });
+
+        return "Message is sent..";
+    }
+}
